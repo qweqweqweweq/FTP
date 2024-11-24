fcf4c4ab409a24eab4e6849d6b19e073511fed29
Pleshkova, Anastasiia A. <Pleshkova.AA@parmalogica.ru>
2024-11-24 23:34:46 +0500

    Созданный метод ConnectServer для подключения к серверу. Плешкова

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Common;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -62,6 +66,93 @@ namespace Client
                 }
             }
             return BCommand;
+        }
+        public static void ConnectServer()
+        {
+            try
+            {
+                IPEndPoint endPoint = new IPEndPoint(IpAddress, Port);
+                Socket socket = new Socket(
+                    AddressFamily.InterNetwork,
+                    SocketType.Stream,
+                    ProtocolType.Tcp);
+                socket.Connect(endPoint);
+
+                if (socket.Connected)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    string message = Console.ReadLine();
+                    
+                    if (CheckCommand(message))
+                    {
+                        ViewModelSend viewModelSend = new ViewModelSend(message, Id);
+
+                        if (message.Split(new string[1] { " " }, StringSplitOptions.None)[0] == "set")
+                        {
+                            string[] DataMessage = message.Split(new string[1] { " " }, StringSplitOptions.None);
+
+                            string NameFile = "";
+                            for (int i = 1; i < DataMessage.Length; i++)
+                                if (NameFile == "")
+                                    NameFile += DataMessage[i];
+                                else
+                                    NameFile += " " + DataMessage[i];
+                            if (File.Exists(NameFile))
+                            {
+                                FileInfo fileInfo = new FileInfo(NameFile);
+                                FileInfoFTP NewFileInfo = new FileInfoFTP(File.ReadAllBytes(NameFile), fileInfo.Name);
+                                viewModelSend = new ViewModelSend(JsonConvert.SerializeObject(NewFileInfo), Id);
+                            }
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Указанный файл не существует");
+                            }
+                        }
+
+                        byte[] messageByte = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(viewModelSend));
+                        int BytesSend = socket.Send(messageByte);
+                        byte[] bytes = new byte[10485760];
+                        int BytesRec = socket.Receive(bytes);
+                        string messageServer = Encoding.UTF8.GetString(bytes, 0, BytesRec);
+                        ViewModelMessage viewModelMessage = JsonConvert.DeserializeObject<ViewModelMessage>(messageServer);
+
+                        if (viewModelMessage.Command == "authorization")
+                            Id = int.Parse(viewModelMessage.Data);
+                        else if (viewModelMessage.Command == "message")
+                            Console.WriteLine(viewModelMessage.Data);
+                        else if (viewModelMessage.Command == "cd")
+                        {
+                            List<string> FoldersFiles = new List<string>();
+                            FoldersFiles = JsonConvert.DeserializeObject<List<string>>(viewModelMessage.Data);
+                            foreach (string Name in FoldersFiles)
+                                Console.WriteLine(Name);
+                        }
+                        else if (viewModelMessage.Command == "file")
+                        {
+                            string[] DataMessage = viewModelSend.Message.Split(new string[1] { " " }, StringSplitOptions.None);
+                            string getFile = "";
+                            for (int i = 1; i < DataMessage.Length; i++)
+                                if (getFile == "")
+                                    getFile = DataMessage[i];
+                                else
+                                    getFile += " " + DataMessage[i];
+                            byte[] byteFile = JsonConvert.DeserializeObject<byte[]>(viewModelMessage.Data);
+                            File.WriteAllBytes(getFile, byteFile);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Подключение не удалось.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Что-то случилось: " + ex.Message);
+            }
         }
     }
 }
