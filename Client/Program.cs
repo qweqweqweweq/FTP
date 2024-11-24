99c7c2751ce0fa4407ecc33bbb3656e046a85d76
Pleshkova, Anastasiia A. <Pleshkova.AA@parmalogica.ru>
2024-11-24 23:17:21 +0500

    Создание метода CheckCommand для проверки сообщений. Плешкова

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,59 @@ namespace Client
 {
     internal class Program
     {
+        public static IPAddress IpAddress;
+        public static int Port;
+        public static int Id = -1;
+
         static void Main(string[] args)
         {
+        }
+
+        public static bool CheckCommand(string message)
+        {
+            bool BCommand = false;
+            string[] DataMessage = message.Split(new string[1] { " " }, StringSplitOptions.None);
+
+            if (DataMessage.Length > 0)
+            {
+                string Command = DataMessage[0];
+                if (Command == "connect")
+                {
+                    if (DataMessage.Length != 3)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Использование: connect [login] [password]\nПример: connect User1 P@ssw0rd");
+                        BCommand = false;
+                    }
+                    else
+                        BCommand = true;
+                }
+                else if (Command == "cd")
+                    BCommand = true;
+                else if (Command == "get")
+                {
+                    if (DataMessage.Length == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Использование: get [NameFile]\nПример: get Test.txt");
+                        BCommand = false;
+                    }
+                    else
+                        BCommand = true;
+                }
+                else if (Command == "set")
+                {
+                    if (DataMessage.Length == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Использование: set [NameFile]\nПример: set Test.txt");
+                        BCommand = false;
+                    }
+                    else
+                        BCommand = true;
+                }
+            }
+            return BCommand;
         }
     }
 }
