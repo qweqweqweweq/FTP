fafcff3f8ec6f29ecf2d7181195cc3a7c49781f4
Pleshkova, Anastasiia A. <Pleshkova.AA@parmalogica.ru>
2024-11-24 23:07:28 +0500

    Созданные классы в Common.

---
+++ Common/ViewModelSend.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class ViewModelSend
+    {
+        public string Message { get; set; }
+        public int Id { get; set; }
+        public ViewModelSend(string message, int id)
+        {
+            Message = message;
+            Id = id;
+        }
+    }
+}
