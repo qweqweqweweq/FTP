0722e314ab782eec9153ac0b4c19c824823e96ed
Kolovratik <Kolovratik@HOME-PC>
2024-11-28 18:31:31 +0300

    Прописанные переменные и метод авторизации

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,15 +1,23 @@
-﻿using System;
+﻿using Server.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 
 namespace Server
 {
     internal class Program
     {
+        public static List<User> Users = new List<User>();
+        public static IPAddress IpAddress;
+        public static int Port;
+
         static void Main(string[] args)
         {
         }
+
+        public static bool AuthorizationUser(string login, string password)
+        {
+            User user = null;
+            user = Users.Find(x => x.login == login && x.password == password);
+            return user != null;
+        }
     }
-}
