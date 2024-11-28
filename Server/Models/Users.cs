dca08acfa34325b93c5b12f681aa91ffab67598b
Kolovratik <Kolovratik@HOME-PC>
2024-11-28 18:25:03 +0300

    Добавление класса отвечающего за данные о пользователях

---
+++ Server/Models/Users.cs
@@ -0,0 +1,17 @@
+﻿namespace Server.Models
+{
+    public class User
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+        public string src { get; set; }
+        public string temp_src { get; set; }
+        public User(string login, string password, string src)
+        {
+            this.login = login;
+            this.password = password;
+            this.src = src;
+            temp_src = src;
+        }
+    }
+}
