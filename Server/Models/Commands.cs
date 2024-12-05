41a9d9e7c00408c80bdf43e10c2d2897a35c3a05
Pleshkova, Anastasiia A. <[anastasiyapl@bk.ru]>
2024-12-05 14:18:57 +0500

    Создание класса Commands.

---
+++ Server/Models/Commands.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Models
+{
+    public class Commands
+    {
+        public int id {  get; set; }
+        public int userId { get; set; }
+        public string command {  get; set; }
+        public Commands() { }
+    }
+}
